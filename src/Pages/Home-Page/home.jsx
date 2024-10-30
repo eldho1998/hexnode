53084a3def10bb6b52fb6caff00ee6b9c44d2a0c
Eldhose Kurian <eldhosekurian9809@gmail.com>
2024-10-30 18:00:24 +0530

    3rd commit

--- src/Pages/Home-Page/home.jsx
+++ src/Pages/Home-Page/home.jsx
@@ -1,13 +1,15 @@
-import './home.css';
-import Navbar from '../../components/Nav-Bar/navbar';
-import FirstSection from '../../Sections/FirstSection/firstsection';
-import SecondSection from '../../Sections/SecondSection/secondsection';
+import "./home.css";
+import Navbar from "../../components/Nav-Bar/navbar";
+import FirstSection from "../../Sections/FirstSection/firstsection";
+import SecondSection from "../../Sections/SecondSection/secondsection";
+import ThirdSection from "../../Sections/ThirdSection/thirdsection";
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
       <FirstSection />
       <SecondSection />
+      <ThirdSection />
     </div>
   );
 };
--- src/components/Nav-Bar/navbar.jsx
+++ src/components/Nav-Bar/navbar.jsx
@@ -1,6 +1,16 @@
-import './navbar.css';
+import "./navbar.css";
 
 const Navbar = () => {
+  window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".NavBar");
+
+    if (window.scrollY > 0) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+
   return (
     <div className="NavBar">
       <div className="nav-first">
